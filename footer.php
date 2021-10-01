2bf907869d42d41510f6ca38692f0b3885c5b025
Nicolas Munoz Alvarez <deswnicom@gmail.com>
2021-10-01 10:34:06 -0500

    Archivo de footer.php agregado

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template del footer.
+ *
+ */
+?>
+        <footer class="site-foote">
+            <div class="footer-site">
+                <?php do_action("redtech_footer_content_site"); ?>
+            </div>
+        </footer>
+        <?php wp_footer(); ?>
+    </body>
+</html>
